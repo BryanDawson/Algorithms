e7a6b6d95d27438e28b80b66f6a9f6152a6b11c3
BryanDawson <free2be@fastmail.fm>
2017-11-27 17:18:54 -0800

    Add in a 'textbook' implementation for lcs_dynamic.  Now to spend some time working that over, possibly with some rewrite for better understanding.

--- LongestCommonSubstring.py
+++ LongestCommonSubstring.py
@@ -5,21 +5,31 @@
 
 """
 
-def lcs_dynamic(str1, str2):
-    """ Returns the length of the longest common substring, and the string itself"""
 
-    print("Not yet implemented\n", str1, '\n', str2)
-    return
+def lcs_dynamic(s1, s2):
+    """ Returns the longest common substring using dynamic programming"""
+
+    m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
+    longest, x_longest = 0, 0
+    for x in range(1, 1 + len(s1)):
+        for y in range(1, 1 + len(s2)):
+            if s1[x - 1] == s2[y - 1]:
+                m[x][y] = m[x - 1][y - 1] + 1
+                if m[x][y] > longest:
+                    longest = m[x][y]
+                    x_longest = x
+            else:
+                m[x][y] = 0
+    return s1[x_longest - longest: x_longest]
 
 
 def main():
     """ Run the test harness for implementations of Longest Common Substring"""
 
-
     string1 = "The next virtue is: Freedom is better than slavery.".split()
     string2 = "Poverty in freedom is better than riches in chains".split()
 
-    lcs_dynamic(string1, string2)
+    print(lcs_dynamic(string1, string2))
 
 
 main()
