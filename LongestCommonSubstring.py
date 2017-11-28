9927b68201d17a279d3c54167c0ddb356d81a760
BryanDawson <free2be@fastmail.fm>
2017-11-27 17:00:10 -0800

    First submission to Algorithms repo

---
+++ LongestCommonSubstring.py
@@ -0,0 +1,25 @@
+""" Implements solution(s) for the Longest Common Substring problem.
+
+    In this case, the "strings" are themselves strings, so in a sense,
+    the 'alphabet' is 'words'.  See the test cases in main() for examples
+
+"""
+
+def lcs_dynamic(str1, str2):
+    """ Returns the length of the longest common substring, and the string itself"""
+
+    print("Not yet implemented\n", str1, '\n', str2)
+    return
+
+
+def main():
+    """ Run the test harness for implementations of Longest Common Substring"""
+
+
+    string1 = "The next virtue is: Freedom is better than slavery.".split()
+    string2 = "Poverty in freedom is better than riches in chains".split()
+
+    lcs_dynamic(string1, string2)
+
+
+main()
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+This repo contains projects exploring various "well known"
+algorithms.  This continues my "code every day" challenge,
+but with 'harder' problems that will often more than one
+day to complete.
+
+Note: I will generally refer to the available references
+for good solutions.  The intent is not to create original
+and unique solutions, rather to practice working through
+these algorithms and understand good implementations.
+
