03040269266d0e1a556d67d49b49d358c365e110
BryanDawson <free2be@fastmail.fm>
2017-12-03 14:45:17 -0800

    Implements Print All Diagonals of a given matrix

---
+++ PrintDiagonals.py
@@ -0,0 +1,47 @@
+"""Implements Print All Diagonals of a given matrix
+
+   Implementation for: http://algorithms.tutorialhorizon.com/print-all-diagonals-of-a-given-matrix/
+
+   NOTE: This implementation deviates from the reference by using zip() in comprehensions
+            and storing the result in a temporary string
+
+"""
+
+
+def printdiagonals(arr):
+    """Prints all diagonals of the input array"""
+
+    output = ""
+
+    # First add the diagonals that correspond to each row
+    for row in range(len(arr)):
+        output += ' '.join([str(arr[drow][dcol]) for drow, dcol
+                            in zip(range(row, -1, -1), range(row+1))])
+        output += '\n'
+
+    # Now add the remaining diagonals that correspond to the columns 1 through n
+    for col in range(1, len(arr[0])):
+        output += ' '.join([str(arr[drow][dcol]) for drow, dcol
+                            in zip(range(len(arr)-1, -1, -1), range(col, len(arr[0])))])
+        output += '\n'
+
+    print(output)
+
+
+def main():
+    """Test harness for print diagonals"""
+
+    rows, cols = 4, 4
+    printdiagonals([[cols*y+x+1 for x in range(cols)] for y in range(rows)])
+    print()
+
+    rows, cols = 1, 1
+    printdiagonals([[cols*y+x+1 for x in range(cols)] for y in range(rows)])
+    print()
+
+    rows, cols = 4, 5
+    printdiagonals([[cols*y+x+1 for x in range(cols)] for y in range(rows)])
+    print()
+
+
+main()
